553122e93f8c427885e588ab0a16e12101836be7
Chandan Mehto <mehtochandan78@gmail.com>
2025-07-12 21:02:55 +0530

    created middlewares

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -43,8 +43,8 @@ const existedUser = await User.findOne({
 if(existedUser){
     throw new ApiError(409, "User with email or username already exists")
 }
-
-const avatarLocalPath = req.files?.avatar?.[0]?.path //=> coding this for fetching avatr of user from cloudinary.
+console.warn(req.files)
+const avatarLocalPath = req.files?.avatar?.[0]?.path //=> coding this for fetching avatar of user from cloudinary.
 const coverImageLocalPath = req.files?.coverImage?.[0]?.path  //=> creating this for fetching images of user's cover image from  cloudinary.
 
 if(!avatarLocalPath){
@@ -170,8 +170,26 @@ const options = {
 
 const logoutUser = asyncHandler(async(req, res)=>{
     await User.findByIdAndUpdate(
-        //TODO: need to combacke here after learning middleware
+        req.user._id,
+        {
+            $set:{
+                refreshToken:undefined,
+            }
+        },
+        {new:true}
+
     )
+
+    const options = {
+        httpOnly:true,
+        secure:process.env.NODE_ENV === "production"
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"))
 })
 
 
@@ -226,9 +244,111 @@ return res
     }
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res)=>{
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findByIdAndDelete(req.user?._id)
+
+    const isPasswordValid = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Old password is incorrect")
+    }
+
+    user.password = newPassword;
+
+    await user.save({validateBeforeSave:false})
+
+    return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"))
+
+})
+
+const getCurrentUser = asyncHandler(async(req, res)=>{
+    return res.status(200).json(new ApiResponse(200, req.user, "Current user details"))
+})
+
+const updateAccountDetails = asyncHandler(async(req, res)=>{
+const {fullname, email} = req.body
+
+if(!fullname || !email){
+    throw new ApiError(400, "Fullname and email are required");
+}
+
+const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+        $set:{
+            fullname,
+            email: email
+        }
+    },
+    {new:true}
+).select("-password -refreshToken")
+
+return res.status(200).json( new ApiResponse(200, user , "Account details updated successfully"))
+})
+
+const updateUserAvatar= asyncHandler(async(req, res)=>{
+   const avatarLocalPath =  req.file?.path
+
+   if(!avatarLocalPath){
+    throw new ApiError(400,"File is required");
+   }
+
+ const avatar =   await uploadOnCloudinary(avatarLocalPath)
+
+ if(!avatar.url){
+    throw new ApiError(500,"Something went wrong while uploading avatar");
+ }
+
+ const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+        $set:{
+            avatar:avatar.url
+        }
+    },
+    {new:true}
+ ).select("-password -refreshToken")
+
+ res.status(200).json(new ApiResponse(200, user, "Avatar updated successfully"))
+})
+
+const updateUserCoverImage= asyncHandler(async(req, res)=>{
+    const coverImageLocalPath =req.file?.path
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400,"File is required");  
+    }
+   const coverImage =  uploadOnCloudinary(coverImageLocalPath)
+
+   if(!coverImage){
+    throw new ApiError(500, "Something went wrong while uploading cover image");
+   }
+   const user  = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+        $set:{
+            coverImage: coverImage.url
+        }
+    },
+    {new:true}
+   ).select("-password -refreshToken")
+
+   return res.status(200).json(new ApiResponse(200, user, "Cover image updated succcessfully"))
+
+})
+
 
 export{
     registerUser ,
     loginUser,
-    refreshAccessToken
+    refreshAccessToken,
+    logoutUser,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
-import { registerUser } from "../controllers/user.controllers.js";
-import {upload} from '../middlewares/multer.middlewares.js'
+import { registerUser, logoutUser } from "../controllers/user.controllers.js";
+import {upload} from '../middlewares/multer.middlewares.js';
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
@@ -17,5 +18,9 @@ router.route("/register").post(
         }
     ]),    //=> we are using fileds because we are required avater and cover image of user.
      registerUser )
+
+     // secured routes
+
+     router.route('/logout').post(verifyJWT, logoutUser)
 
 export default router;
